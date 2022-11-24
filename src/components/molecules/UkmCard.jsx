dccd40db38369efeaf7e85f1c7125a2e26746573
Ramadandev <dan.alphadev@gmail.com>
2022-11-24 21:18:57 +0700

    re-design carousel for mobile view

--- src/components/molecules/UkmCard.jsx
+++ src/components/molecules/UkmCard.jsx
@@ -2,17 +2,27 @@ import { CheckLabel } from "../atoms";
 
 const UkmCard = ({ title, socialMedia, image }) => {
   return (
-    <div className="w-full max-w-sm bg-white select-none dark:bg-slate-900 h-80 transition-all duratiion-300 flex items-center flex-col gap-4 justify-center hover:shadow-md shadow-blue-300  rounded-xl border border-blue-600">
-      <div className="w-44 h-[10.688rem] bg-[url('../../assets/images/ukmborder.png')] bg-no-repeat object-cover flex items-center px-[0.90rem]">
-        <div className="w-[8.875rem] h-[8.875rem] relative bg-brand bg-opacity-20 rounded-full flex items-center justify-center">
+    <div className="w-full bg-white select-none px-4 dark:bg-brand dark:bg-opacity-10 lg:h-80 transition-all duratiion-300 flex items-center lg:flex-col lg:gap-6 lg:justify-center hover:shadow-md shadow-blue-300 rounded-xl border dark:border-none border-blue-600">
+      <div className="lg:w-44 lg:h-[10.688rem] w-28 h-32 lg:bg-[url('../../assets/images/ukmborder.png')] bg-no-repeat object-cover flex items-center px-[0.90rem]">
+        <div className="lg:w-[8.875rem] w-20 h-20 lg:h-[8.875rem] relative bg-brand bg-opacity-20 rounded-full flex items-center justify-center">
           <div className="w-full h-full flex items-center justify-center overflow-hidden rounded-full">
             <img src={image} alt="ukm-image" />
           </div>
           <CheckLabel />
         </div>
       </div>
-      <h1 className="text-2xl font-semibold dark:text-white">{title}</h1>
-      <h1 className="text-base font-medium text-gray-400">{socialMedia}</h1>
+      <div className="flex flex-col gap-2 lg:gap-0 lg:text-center">
+        <h1 className="lg:text-2xl text-md font-semibold dark:text-white">
+          {title}
+        </h1>
+        <a
+          href={`https://instagram.com/${socialMedia.split("@")[1]}`}
+          target="_blank"
+          className="lg:text-base text-xs hover:underline cursor-pointer font-medium hover:text-gray-700 text-gray-400"
+        >
+          Lihat Instagram
+        </a>
+      </div>
     </div>
   );
 };
